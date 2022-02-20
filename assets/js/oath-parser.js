d8fb43af12747d0dff8c5087047a4098f64869a1
Andreas Bock Michelsen <anbmich@hotmail.com>
2022-02-21 00:47:52 +0100

    Update oath parser version

--- assets/js/oath-parser.js
+++ assets/js/oath-parser.js
@@ -8,7 +8,7 @@ const worldSuitArray = [0,0,0,0,0,0]
 const dispSuitArray = [0,0,0,0,0,0]
 
 // Input savestring
-export const savestring = '030303000219The Helichrysum Chronicle2B010101234505FFFFFF20FFFFFF14FFFFDE1BFFFFFF29FFFFFF16FFFFE01EFFFFFF1FFFFFFF3B0624182B0E2629D61ED31D15D51309522F201C3521001FD2D42819C30B1632AD0F342233C125089810010D112C121A0507042314270AB717312D30062A1B2E0C030212E9DBDCEAE8E7E4DFDADDEDE2E6ECE3E5E1EB000105ARman'
+const savestring = '030303000219The Helichrysum Chronicle2B010101234505FFFFFF20FFFFFF14FFFFDE1BFFFFFF29FFFFFF16FFFFE01EFFFFFF1FFFFFFF3B0624182B0E2629D61ED31D15D51309522F201C3521001FD2D42819C30B1632AD0F342233C125089810010D112C121A0507042314270AB717312D30062A1B2E0C030212E9DBDCEAE8E7E4DFDADDEDE2E6ECE3E5E1EB000105ARman'
 
 // --------------- Initialize and specify
 constructFormat()
@@ -27,7 +27,7 @@ const save = {
   gameCount: hexParser(3),
   nameLength: hexParser(4),
   name: strParser(5),
-  // 6 is unused
+  prevActiveStatus: hexParser(6),
   exileCitizenStatus: hexParser(7),
   oath: other.oathArray[hexParser(8)],
   // Skip suit order (???), 9-15.
@@ -48,6 +48,8 @@ const save = {
   prevWinNameLength: hexParser(52+offset),
   prevWinName: strParser(53+offset)
 }
+
+//console.log(save)
 
 export {save, worldSuitArray, dispSuitArray}
 

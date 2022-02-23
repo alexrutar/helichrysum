50cbefd15962de12460067b1fa2be9dfcd26e141
Alex Rutar <alex@rutar.org>
2022-02-23 01:22:38 +0000

    improve javascript
    - simplify code to have less repetition
    - rename some variables to be a bit more consistent
    - change site save structure to use array instead of named elements

--- assets/js/oath-parser.js
+++ assets/js/oath-parser.js
@@ -20,143 +20,134 @@ const offset = worldDeckSize + dispossessedDeckSize + relicDeckSize
 
 // --------------- Assign to object
 const save = {
-  // ---------------- Base
-  majorVersion: hexParser(0),
-  minorVersion: hexParser(1),
-  patch: hexParser(2),
-  gameCount: hexParser(3),
-  nameLength: hexParser(4),
-  name: strParser(5),
-  prevActiveStatus: hexParser(6),
-  exileCitizenStatus: citizenParser(7),
-  oath: other.oathArray[hexParser(8)],
-  // Skip suit order (???), 9-15.
-  // ---------------- Map
-  site1: parseSite(15),
-  site2: parseSite(19),
-  site3: parseSite(23),
-  site4: parseSite(27),
-  site5: parseSite(31),
-  site6: parseSite(35),
-  site7: parseSite(39),
-  site8: parseSite(43),
-  worldDeck: parseSuitDeck(47, worldSuitArray, worldDeckSize),
-  dispDeck: parseSuitDeck(48+worldDeckSize, dispSuitArray, dispossessedDeckSize),
-  relicDeck: parseRelicDeck(49+worldDeckSize+dispossessedDeckSize, relicDeckSize),
-  prevCitizenStatus: hexParser(50+offset),
-  prevWinColor: other.winCases[hexParser(51+offset)],
-  prevWinNameLength: hexParser(52+offset),
-  prevWinName: strParser(53+offset)
+    // ---------------- Base
+    majorVersion: hexParser(0),
+    minorVersion: hexParser(1),
+    patch: hexParser(2),
+    gameCount: hexParser(3),
+    nameLength: hexParser(4),
+    name: strParser(5),
+    prevActiveStatus: hexParser(6),
+    exileCitizenStatus: citizenParser(7),
+    oath: other.oathArray[hexParser(8)],
+    // Skip suit order (???), 9-15.
+    // ---------------- Map
+    sites: [parseSite(15), parseSite(19), parseSite(23), parseSite(27), parseSite(31), parseSite(35), parseSite(39), parseSite(43)],
+    worldDeck: parseSuitDeck(47, worldSuitArray, worldDeckSize),
+    dispDeck: parseSuitDeck(48+worldDeckSize, dispSuitArray, dispossessedDeckSize),
+    relicDeck: parseRelicDeck(49+worldDeckSize+dispossessedDeckSize, relicDeckSize),
+    prevCitizenStatus: hexParser(50+offset),
+    prevWinColor: other.winCases[hexParser(51+offset)],
+    prevWinNameLength: hexParser(52+offset),
+    prevWinName: strParser(53+offset)
 }
-
-//console.log(save) // Beware of spoilers when printing to console!
 
 export {save, worldSuitArray, dispSuitArray}
 
 // --------------- Functions
 
 function parseRelicDeck(n,size){
-  // Keep track of suit numbers
-  const deck = []
-  for (let i = 0; i < size; i++ ){
-    const cardName = CardNameIndexes[hexParser(n+i+1)]
-    deck.push(cardName)
-  }
-  return deck
+    // Keep track of suit numbers
+    const deck = []
+    for (let i = 0; i < size; i++ ){
+        const cardName = CardNameIndexes[hexParser(n+i+1)]
+        deck.push(cardName)
+    }
+    return deck
 }
 
 function parseSuitDeck(n,array,size){
-  // Keep track of suit numbers
-  const deck = []
-  for (let i = 0; i < size; i++ ){
-    const cardName = CardNameIndexes[hexParser(n+i+1)]
-    deck.push(cardName)
-    if ( CardSuits[cardName] != undefined ){
-      // Avoid errors from visions
-      array[CardSuits[cardName]] += 1
+    // Keep track of suit numbers
+    const deck = []
+    for (let i = 0; i < size; i++ ){
+        const cardName = CardNameIndexes[hexParser(n+i+1)]
+        deck.push(cardName)
+        if ( CardSuits[cardName] != undefined ){
+            // Avoid errors from visions
+            array[CardSuits[cardName]] += 1
+        }
     }
-  }
-  return deck
+    return deck
 }
 
 
 function parseSite(n){
-  const site = {
-    name: SiteNameIndexes[hexParser(n)],
-    index1: hexParser(n+1),
-    card1: CardNameIndexes[hexParser(n+1)],
-    index2:hexParser(n+2),
-    card2: CardNameIndexes[hexParser(n+2)],
-    index3: hexParser(n+3),
-    card3: CardNameIndexes[hexParser(n+3)]
-  }
-  return site
+    const site = {
+        name: SiteNameIndexes[hexParser(n)],
+        index1: hexParser(n+1),
+        card1: CardNameIndexes[hexParser(n+1)],
+        index2:hexParser(n+2),
+        card2: CardNameIndexes[hexParser(n+2)],
+        index3: hexParser(n+3),
+        card3: CardNameIndexes[hexParser(n+3)]
+    }
+    return site
 }
 
 function constructFormat(){
-  format.push(...[2,2,2,4,2,0,2,2,2,1,1,1,1,1,1]) // Base
-  format[5] = hexParser(4) // Add chronicle name length
+    format.push(...[2,2,2,4,2,0,2,2,2,1,1,1,1,1,1]) // Base
+    format[5] = hexParser(4) // Add chronicle name length
 
-  const sitef = [2,2,2,2] // Site
-  format.push(...sitef.concat(sitef, sitef, sitef, sitef, sitef, sitef, sitef)) // 8 sites on map
+    const sitef = [2,2,2,2] // Site
+    format.push(...sitef.concat(sitef, sitef, sitef, sitef, sitef, sitef, sitef)) // 8 sites on map
 
-  // World
-  format.push(2)
-  worldDeckSize = hexParser(format.length-1)
-  addCardsFormat(worldDeckSize)
+    // World
+    format.push(2)
+    worldDeckSize = hexParser(format.length-1)
+    addCardsFormat(worldDeckSize)
 
-  // Dispossessed
-  format.push(2)
-  dispossessedDeckSize = hexParser(format.length-1)
-  addCardsFormat(dispossessedDeckSize)
+    // Dispossessed
+    format.push(2)
+    dispossessedDeckSize = hexParser(format.length-1)
+    addCardsFormat(dispossessedDeckSize)
 
-  // Relics
-  format.push(2)
-  relicDeckSize = hexParser(format.length-1)
-  addCardsFormat(relicDeckSize)
+    // Relics
+    format.push(2)
+    relicDeckSize = hexParser(format.length-1)
+    addCardsFormat(relicDeckSize)
 
-  // Previous game
-  format.push(...[2,2,2])
-  format.push(hexParser(format.length-1))
+    // Previous game
+    format.push(...[2,2,2])
+    format.push(hexParser(format.length-1))
 }
 
 function addCardsFormat(deckSize){
-  for (let i = 0; i < deckSize; i++){
-    format.push(2);
-  }
+    for (let i = 0; i < deckSize; i++){
+        format.push(2);
+    }
 }
 
 function citizenParser(n){
-  var m = getPos(n)
-  const byte = parseInt(savestring.substr(m,format[n]))
-  // Order of colors in byte: [] [] [] [Black] [Yellow] [White] [Blue] [Red]
-  const citizenStatus = {
-    red: byte & 1 ? "citizen" : "exile", // Bitwise magic and ternary operators!
-    blue: byte & 2 ? "citizen" : "exile",
-    white: byte & 4 ? "citizen" : "exile",
-    yellow: byte & 8 ? "citizen" : "exile",
-    black: byte & 16 ? "citizen" : "exile"
-  }
-  return citizenStatus
+    var m = getPos(n)
+    const byte = parseInt(savestring.substr(m,format[n]))
+    // Order of colors in byte: [] [] [] [Black] [Yellow] [White] [Blue] [Red]
+    const citizenStatus = {
+        red: byte & 1 ? "citizen" : "exile", // Bitwise magic and ternary operators!
+        blue: byte & 2 ? "citizen" : "exile",
+        white: byte & 4 ? "citizen" : "exile",
+        yellow: byte & 8 ? "citizen" : "exile",
+        black: byte & 16 ? "citizen" : "exile"
+    }
+    return citizenStatus
 }
 
 function strParser(n){
-  var m = getPos(n)
-  return savestring.substr(m,format[n])
+    var m = getPos(n)
+    return savestring.substr(m,format[n])
 }
 
 function hexParser(n){
-  assert(n<format.length,'Index beyond savestring length.')
-  var m = getPos(n)
-  return parseInt(savestring.substr(m,format[n]),16)
+    assert(n<format.length,'Index beyond savestring length.')
+    var m = getPos(n)
+    return parseInt(savestring.substr(m,format[n]),16)
 }
 
 function getPos(n){
-  var m = 0;
-  for (let i=0; i<n; i++){
-    m = m + format[i];
-  }
-  return m
+    var m = 0;
+    for (let i=0; i<n; i++){
+        m = m + format[i];
+    }
+    return m
 }
 
 function assert(condition, message) {
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 import {savestring, save, worldSuitArray, dispSuitArray} from './oath-parser.js'
 
+
 // set background image based on previous winner
 if (window.innerWidth >= 1200) {
-    if (save.prevWinColor == "purple") {
+    if (save.prevWinColor == "chancellor") {
         var img_name = "full_chancellor.png";
     } else {
         var img_name = "full_" + save.exileCitizenStatus[save.prevWinColor] + "_" + save.prevWinColor + ".png"
@@ -11,103 +12,96 @@ if (window.innerWidth >= 1200) {
 }
 
 
-// Automatically insert a few things into the webpage
+// insert savestring
+document.getElementById("savestring").innerHTML = savestring + "\n";
+
+
+// insert site names
+function insertSiteName(id, site){
+    if (site.name != undefined){
+        var string = '</br>- ' + site.name;
+        if (site.index1 < 210){
+            string += ': '+ site.card1;}
+        if (site.index2 < 210){
+            string += ', '+ site.card2;}
+        if (site.index3 < 210){
+            string += ', '+ site.card3;}
+        document.getElementById(id).innerHTML = string
+    }
+}
 document.getElementById("name").innerHTML = save.name;
-insertSiteName("site1",save.site1);
-insertSiteName("site2",save.site2);
-insertSiteName("site3",save.site3);
-insertSiteName("site4",save.site4);
-insertSiteName("site5",save.site5);
-insertSiteName("site6",save.site6);
-insertSiteName("site7",save.site7);
-insertSiteName("site8",save.site8);
+for (let i=1; i<9; i++) {
+    insertSiteName("site" + i, save.sites[i-1]);
+}
+
 
 // Insert current oath
-switch(save.oath){
-    case "Supremacy":
-        document.getElementById("oath").innerHTML = '<img src="assets/images/supremacy.png"> <span class="goudy-capital">O</span>ath <em>of</em> <span class="goudy-capital">S</span>upremacy';
-        break;
-    case "People":
-        document.getElementById("oath").innerHTML = '<img src="assets/images/people.png"> <span class="goudy-capital">O</span>ath <em>of</em> <span class="goudy-capital">T</span>he <span class="goudy-capital">P</span>eople';
-        break;
-    case "Devotion":
-        document.getElementById("oath").innerHTML = '<img src="assets/images/devotion.png"> <span class="goudy-capital">O</span>ath <em>of</em> <span class="goudy-capital">D</span>evotion';
-        break;
-    case "Protection":
-        document.getElementById("oath").innerHTML = '<img src="assets/images/protection.png"> <span class="goudy-capital">O</span>ath <em>of</em> <span class="goudy-capital">S</span>anctuary';
-        break;
+const oathFancyName = {
+    "supremacy": '<span class="goudy-capital">S</span>upremacy',
+    "people": '<span class="goudy-capital">T</span>he <span class="goudy-capital">P</span>eople',
+    "devotion": '<span class="goudy-capital">D</span>evotion',
+    "protection": '<span class="goudy-capital">P</span>rotection',
+    "conspiracy": '<span class="goudy-capital">C</span>onspiracy'
 }
+document.getElementById("oath").innerHTML = '<img src="assets/images/' + save.oath + '.png"> <span class="goudy-capital">O</span>ath <em>of</em> ' + oathFancyName[save.oath];
 
-// Insert suit numbers
+
+// insert suit numbers
 var number_tags = document.getElementsByClassName('number');
+// TODO: this is pretty fragile since it depends on the table order not
+// changing, which feels like a bad idea
 for (var j=0;j<number_tags.length;j++){
-var script_tag = number_tags[j]
+    var script_tag = number_tags[j];
     const count = worldSuitArray[j];
-
-    script_tag.innerHTML += count;
+    script_tag.innerHTML = count;
 }
 
-// Automatically visualize the suit distribution with a bar plot
+// visualize the suit distribution with a bar plot
 var counter_tags = document.getElementsByClassName('counter');
 for (var j=0;j<counter_tags.length;j++){
-var script_tag = counter_tags[j]
-  var imgsrc = script_tag.getAttribute("data-imgsrc");
-  const count = worldSuitArray[j];
+    var script_tag = counter_tags[j];
+    var imgsrc = script_tag.getAttribute("data-imgsrc");
+    const count = worldSuitArray[j];
 
-  script_tag.innerHTML += '<p>';
-  for (var i = 1; i <= count; i++) {
-    script_tag.innerHTML += '<img src="' + imgsrc + '">';
-  }
-  script_tag.innerHTML += '</p>';
+    for (var i = 1; i <= count; i++) {
+        script_tag.innerHTML += '<img src="' + imgsrc + '">';
+    }
 }
 
 // Insert a random suit quote in the footer
 (function() {
-  var quotes = [
-    {
-      text: " The home, a crackling fire. Calm, contentedness, and ease. ",
-      img: "assets/images/suit-hearth.png"
-    },
-    {
-      text: " Bright lights in the night sky. Starry-eyed discovery and esoteric tradition. ",
-      img: "assets/images/suit-arcane.png"
-    },
-    {
-      text: " The betrayal of a sibling. The sewers, rats chewing on spare bones. ",
-      img: "assets/images/suit-discord.png"
-    },
-    {
-      text: " Sword, stone, and burnished silver. Lockstep, willing or unwilling. ",
-      img: "assets/images/suit-order.png"
-    },
-    {
-      text: " Scratching, rustling in the grass. Fur, scale, and claw. ",
-      img: "assets/images/suit-beast.png"
-    },
-    {
-      text: " The sun, the moon—those traveling bodies. Care for one’s own. ",
-      img: "assets/images/suit-nomad.png"
-    }
-  ];
-  var quote = quotes[Math.floor(Math.random() * quotes.length)];
-  // Get the Quote element from the footer and insert the HTML
-  document.getElementById("quote").innerHTML =
-    '<p>' +
-    '<img src="' + quote.img + '">'+
-    quote.text +
-    '<img src="' + quote.img + '">' +
-    '</p>';
+    var quotes = [
+        {
+            text: " The home, a crackling fire. Calm, contentedness, and ease. ",
+            img: "assets/images/suit-hearth.png"
+        },
+        {
+            text: " Bright lights in the night sky. Starry-eyed discovery and esoteric tradition. ",
+            img: "assets/images/suit-arcane.png"
+        },
+        {
+            text: " The betrayal of a sibling. The sewers, rats chewing on spare bones. ",
+            img: "assets/images/suit-discord.png"
+        },
+        {
+            text: " Sword, stone, and burnished silver. Lockstep, willing or unwilling. ",
+            img: "assets/images/suit-order.png"
+        },
+        {
+            text: " Scratching, rustling in the grass. Fur, scale, and claw. ",
+            img: "assets/images/suit-beast.png"
+        },
+        {
+            text: " The sun, the moon—those traveling bodies. Care for one’s own. ",
+            img: "assets/images/suit-nomad.png"
+        }
+    ];
+    var quote = quotes[Math.floor(Math.random() * quotes.length)];
+    // Get the Quote element from the footer and insert the HTML
+    document.getElementById("quote").innerHTML =
+        '<p>' +
+        '<img src="' + quote.img + '">'+
+        quote.text +
+        '<img src="' + quote.img + '">' +
+        '</p>';
 })();
-
-function insertSiteName(id,site){
-if (site.name != undefined){
-	var string = '</br>- ' + site.name;
-	if (site.index1 < 210){
-		string += ': '+ site.card1;}
-	if (site.index2 < 210){
-		string += ', '+ site.card2;}
-	if (site.index3 < 210){
-		string += ', '+ site.card3;}
-	document.getElementById(id).innerHTML = string
-	}
-}
